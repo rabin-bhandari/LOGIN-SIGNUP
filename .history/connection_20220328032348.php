36b40b7cff7e41bf6130838ef2c88d08dd34effe
rabin-bhandari <63465803+rabin-bhandari@users.noreply.github.com>
2022-03-28 03:39:50 +0100

    clear db settings

--- .history/connection_20220328032348.php
+++ .history/connection_20220328032348.php
@@ -13,7 +13,7 @@
 
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b49407036ab3d6:dcca5948@eu-cdbr-west-02.cleardb.net/heroku_6f25953872e8cc7?reconnect=true"));
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
@@ -22,3 +22,15 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?><?php
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    ?>
